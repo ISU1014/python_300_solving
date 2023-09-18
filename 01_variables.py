4966b6c64a5beea3b19b97d83fc6c9a00a028ba1
Isu Ryu <61234971+ISU1014@users.noreply.github.com>
2023-09-18 16:39:30 +0900

    파이썬_300제_개인풀이

---
+++ 01_variables.py
@@ -0,0 +1,44 @@
+print("Hello world")
+print("mary's cosmetics")
+print("\"도둑이야\"")
+print("C:\\Windows")
+print("""안녕하세요
+      만나서        반갑습니다""")
+print("오늘은","월요일")
+print("오늘은"+"월요일")
+print("naver;kakao;sk;samsung")
+print("first",end=" ");print("second")
+
+print("="*50)
+
+시가총액 = 2980000000
+현재가 = 5000
+PER = 15.79
+print(시가총액, type(시가총액))
+print(현재가, type(현재가))
+print(PER, type(PER))
+
+num_str = "720"
+print(int(num_str))
+
+num = 100
+result = str(num)
+print(result)
+print(type(result))
+
+num = "15.79"
+result = float(num)
+print(num)
+
+year = "2020"
+result = int(year)
+print(result,result-1,result-2)
+
+monthly = 48584
+leftMonth = 36
+print(monthly+leftMonth)
+
+
+
+
+
---
+++ 02_string.py
@@ -0,0 +1,6 @@
+letters ="python"
+print(letters[0],letters[2])
+
+license_plate = "24가 2210"
+print(license_plate[4:8])
+
